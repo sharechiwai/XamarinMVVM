d1eec874f5106032a95370ba7433a90000f66a2b
Chi <yau.chiwai@gmail.com>
2016-10-07 22:26:49 +0800

    Complete source code of https://developer.xamarin.com/guides/xamarin-forms/xaml/xaml-basics/data_bindings_to_mvvm/

---
+++ XamarinMVVM/XamarinMVVM/ViewModels/ClockViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinMVVM.ViewModels
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+        DateTime dateTime;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ClockViewModel()
+        {
+            this.DateTime = DateTime.Now;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                this.DateTime = DateTime.Now;
+                return true;
+            });
+        }
+
+        public DateTime DateTime
+        {
+            set
+            {
+                if (dateTime != value)
+                {
+                    dateTime = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this,
+                            new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+            }
+            get
+            {
+                return dateTime;
+            }
+        }
+
+    }
+}
